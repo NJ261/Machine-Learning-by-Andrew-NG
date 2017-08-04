a4df12d483753b353c245d5d7d16f7754e139ce5
Nirav Jadeja <nirav9033@gmail.com>
2017-08-04 15:46:23 +0530

    Update gradientDescentMulti.m

--- ex1/ex1/gradientDescentMulti.m
+++ ex1/ex1/gradientDescentMulti.m
@@ -21,7 +21,6 @@ for iter = 1:num_iters
 
     theta = theta - alpha * delta;
     
-
     % ============================================================
 
     % Save the cost J in every iteration    
